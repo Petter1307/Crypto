403f30607ef7d90c996ade0c6b32a664cb8e9303
Petter1307 <petrealupu75@gmail.com>
2022-06-10 19:50:39 +0300

    Test commit, nothing imoportant modified

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
                     BCH
                   </span>
                   <span className="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-                    CHD
+                    CHD xd
                   </span>
                 </div>
                 {/* <div className="text-sm text-red-600">
