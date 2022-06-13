84e53dd97399aec10460adf39b7e88e2492af427
Petter1307 <petrealupu75@gmail.com>
2022-06-13 23:27:00 +0300

    Finished basic version of crypto app

---
+++ src/Components/HorizontalLine.js
@@ -0,0 +1,4 @@
+const HR = () => {
+  return <hr className="w-full border-t border-gray-600 my-4" />;
+};
+export default HR;
---
+++ src/Components/Input.js
@@ -0,0 +1,22 @@
+const Input = () => {
+  return (
+    <>
+      <label
+        htmlFor="wallet"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Ticker
+      </label>
+      <div className="mt-1 relative rounded-md shadow-md">
+        <input
+          type="text"
+          name="wallet"
+          id="wallet"
+          className="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+          placeholder="Example DOGE"
+        />
+      </div>
+    </>
+  );
+};
+export default Input;
